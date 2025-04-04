afc9a780959a0918201f44c563590ecb2ccdd266
Vinu03700 <vinaykalasannavar124@gmail.com>
2025-04-04 11:23:17 +0530

    first commit

---
+++ pgm.java
@@ -0,0 +1,10 @@
+public class swap{
+    public static void main(String[] args){
+        int temp,a=10,b=20;
+        system.out.println("Before swaping a="+a+"b="+b);
+        temp=a;
+        a=b;
+        b=temp;
+                system.out.println("After swaping a="+a+"b="+b);
+    }
+}

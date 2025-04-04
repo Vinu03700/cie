05ca3d2bc3ad1ce1c1c348557ba949a9634d01b9
vinu033 <vinaykalasannavar124@gmail.com>
2025-04-04 11:17:12 +0530

    Update pgm.java

--- pgm.java
+++ pgm.java
@@ -1,9 +1,10 @@
-public class pgm
-{
-    public static void main(string [] args)
-    {
-        int mul=2*3;
-        system.out.println(mul);
-         system.out.println("Hello Bike");
+public class swap{
+    public static void main(String[] args){
+        int temp,a=10,b=20;
+        system.out.println("Before swaping a="+a+"b="+b);
+        temp=a;
+        a=b;
+        b=temp;
+                system.out.println("After swaping a="+a+"b="+b);
     }
 }
